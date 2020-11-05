7d704617f6b6690fce166b3eb5ad470b955d7cb8
AliRahimi <seyedalirahimi76@gmail.com>
2020-11-05 16:32:37 +0330

    change code

--- main.py
+++ main.py
@@ -26,34 +26,36 @@ if __name__ == '__main__':
     input_folder = './RAWDATA/'
     files = glob.glob(f'{input_folder}*.csv')
     for file in files:
-        print(file)
-        data = pd.read_csv(file)
+        try:
+            print(file)
+            data = pd.read_csv(file)
 
-        TICKER = data[TICKER_INDEX].iloc[-HISTORY_LENGTH:]
-        Opens = data[OPEN_INDEX].iloc[-HISTORY_LENGTH:]
-        Highs = data[HIGH_INDEX].iloc[-HISTORY_LENGTH:]
-        Lows = data[LOW_INDEX].iloc[-HISTORY_LENGTH:]
-        Closes = data[CLOSE_INDEX].iloc[-HISTORY_LENGTH:]
-        Volumes = data[VOl_INDEX].iloc[-HISTORY_LENGTH:]
-        Values = data[VALUE_INDEX].iloc[-HISTORY_LENGTH:]
-        DATE = data[DATE_INDEX].iloc[-HISTORY_LENGTH:]
+            TICKER = data[TICKER_INDEX].iloc[-HISTORY_LENGTH:]
+            Opens = data[OPEN_INDEX].iloc[-HISTORY_LENGTH:]
+            Highs = data[HIGH_INDEX].iloc[-HISTORY_LENGTH:]
+            Lows = data[LOW_INDEX].iloc[-HISTORY_LENGTH:]
+            Closes = data[CLOSE_INDEX].iloc[-HISTORY_LENGTH:]
+            Volumes = data[VOl_INDEX].iloc[-HISTORY_LENGTH:]
+            Values = data[VALUE_INDEX].iloc[-HISTORY_LENGTH:]
+            DATE = data[DATE_INDEX].iloc[-HISTORY_LENGTH:]
 
-        rsi = RSI(Closes)
+            rsi = RSI(Closes)
 
-        name = TICKER.to_numpy()[-1]
-        out = HD_POSITIVE().RD(Lows, rsi, DATE)
-        if len(out) != 0:
-            HD_POS.append([name, out])
-        out = HD_NEGATIVE().RD(Highs, rsi, DATE)
-        if len(out) != 0:
-            HD_NEG.append([name, out])
-        out = RD_NEGATIVE().RD(Highs, rsi, DATE)
-        if len(out) != 0:
-            RD_NEG.append([name, out])
-        out = RD_POSITIVE().RD(Lows, rsi, DATE)
-        if len(out) != 0:
-            RD_POS.append([name, out])
-
+            name = TICKER.to_numpy()[-1]
+            out = HD_POSITIVE().RD(Lows, rsi, DATE)
+            if len(out) != 0:
+                HD_POS.append([name, out])
+            out = HD_NEGATIVE().RD(Highs, rsi, DATE)
+            if len(out) != 0:
+                HD_NEG.append([name, out])
+            out = RD_NEGATIVE().RD(Highs, rsi, DATE)
+            if len(out) != 0:
+                RD_NEG.append([name, out])
+            out = RD_POSITIVE().RD(Lows, rsi, DATE)
+            if len(out) != 0:
+                RD_POS.append([name, out])
+        except:
+            print('except')
     pd.DataFrame(HD_POS).to_csv('OUTPUT/HD_POS.csv')
     pd.DataFrame(HD_NEG).to_csv('OUTPUT/HD_NEG.csv')
     pd.DataFrame(RD_POS).to_csv('OUTPUT/RD_POS.csv')
